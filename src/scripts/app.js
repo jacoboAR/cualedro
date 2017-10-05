7803be1f3240452611f9bdf731be33baecaec059
Jacobo Araujo <info@jacoboaraujo.com>
2017-10-05 23:04:43 +0200

    First commit

---
+++ src/scripts/app.js
@@ -0,0 +1,501 @@
+const locations = [{
+        title: '<h6>Xacemento romano de St. Marta</h6>',
+        location: {
+            lat: 41.9414509,
+            lng: -7.6107922
+        }
+    },
+    {
+        title: '<h6>Castro da Saceda</h6>',
+        location: {
+            lat: 41.9315844,
+            lng: -7.6271886
+        }
+    },
+    {
+        title: '<h6>Mámoa do Madorriño</h6>',
+        location: {
+            lat: 41.928232,
+            lng: -7.6120716
+        }
+    },
+    {
+        title: '<h6>Petróglifo da Ronca</h6>',
+        location: {
+            lat: 41.9089893,
+            lng: -7.6576893
+        }
+    },
+    {
+        title: '<h6>Catro de San Millao</h6>',
+        location: {
+            lat: 41.90393,
+            lng: -7.6054144
+        }
+    },
+    {
+        title: '<h6>Miliarios de Lamas</h6>',
+        location: {
+            lat: 41.9822118,
+            lng: -7.5403816
+        }
+    },
+    {
+        title: '<h6>A Cidá de Carzoá</h6>',
+        location: {
+            lat: 41.9661754,
+            lng: -7.5829912
+        }
+    },
+    {
+        title: '<h6>Fonte da Rúa da Igrexa</h6>',
+        location: {
+            lat: 41.892088,
+            lng: -7.5990334
+        }
+    },
+    {
+        title: '<h6>Petróglifo da Cheira</h6>',
+        location: {
+            lat: 41.8974228,
+            lng: -7.6689264
+        }
+    },
+    {
+        title: '<h6>Mámoa da Cheira</h6>',
+        location: {
+            lat: 41.8979229,
+            lng: -7.667242
+        }
+    },
+
+    {
+        title: '<h6>Igrexa de Santa María</h6',
+        location: {
+            lat: 41.8915179,
+            lng: -7.6002672
+        }
+    },
+    {
+        title: '<h6>Igrexa Parroquial de San Salvador</h6',
+        location: {
+            lat: 41.9000451,
+            lng: -7.6376519
+        }
+    },
+    {
+        title: '<h6>Casa Reitoral de Atás</h6',
+        location: {
+            lat: 42.0225999,
+            lng: -7.5723213
+        }
+    },
+    {
+        title: '<h6>Igrexa Parroquial de Atás</h6',
+        location: {
+            lat: 42.0223907,
+            lng: -7.5718975
+        }
+    },
+    {
+        title: '<h6>Ermida da Saúde</h6',
+        location: {
+            lat: 42.0180687,
+            lng: -7.5598142
+        }
+    },
+    {
+        title: '<h6>Igrexa Vella de Cualedro</h6',
+        location: {
+            lat: 41.9902943,
+            lng: -7.5939292
+        }
+    },
+    {
+        title: '<h6>Igrexa de San Bartolomeu de Baldriz</h6',
+        location: {
+            lat: 42.0315658,
+            lng: -7.5281695
+        }
+    },
+    {
+        title: '<h6>Igrexa de Sta María das Mercedes</h6',
+        location: {
+            lat: 42.065545,
+            lng: -7.5399832
+        }
+    },
+    {
+        title: '<h6>Igrexa de San Roque</h6',
+        location: {
+            lat: 41.9647973,
+            lng: -7.5845549
+        }
+    },
+    {
+        title: '<h6>Igrexa da Lucenza</h6',
+        location: {
+            lat: 41.9529796,
+            lng: -7.6212958
+        }
+    },
+    {
+        title: '<h6>Capela da Xironda</h6',
+        location: {
+            lat: 41.9016282,
+            lng: -7.63722
+        }
+    },
+    {
+        title: '<h6>Capela de San Ciríaco</h6',
+        location: {
+            lat: 41.9229956,
+            lng: -7.645551
+        }
+    },
+    {
+        title: '<h6>Capela de Sta Marta</h6',
+        location: {
+            lat: 41.9412115,
+            lng: -7.6108378
+        }
+    },
+    {
+        title: '<h6>Capela de San Martiño</h6',
+        location: {
+            lat: 41.9669452,
+            lng: -7.5529718
+        }
+    },
+    {
+        title: '<h6>Igrexa de Sta Baia de Montes</h6',
+        location: {
+            lat: 41.9649888,
+            lng: -7.5548092
+        }
+    },
+    {
+        title: '<h6>Igrexa de San Pedro de Pena Verde</h6',
+        location: {
+            lat: 41.9950179,
+            lng: -7.5653596
+        }
+    },
+    {
+        title: '<h6>Igrexa de San Martiño de Rebordondo</h6',
+        location: {
+            lat: 41.9932048,
+            lng: -7.5388983
+        }
+    },
+    {
+        title: '<h6>Igrexa da Moimenta</h6',
+        location: {
+            lat: 41.97151,
+            lng: -7.6109183
+        }
+    },
+    {
+        title: '<h6>Igrexa de Santiago de Vilela</h6',
+        location: {
+            lat: 41.9660279,
+            lng: -7.619842
+        }
+    },
+    {
+        title: '<h6>Igrexa de San Salvador de Vilar de Lebres</h6',
+        location: {
+            lat: 41.989754,
+            lng: -7.6159435
+        }
+    },
+
+    {
+        title: '<h6>Lenda da Mao Negra</h6',
+        location: {
+            lat: 41.989442,
+            lng: -7.5943355
+        }
+    },
+    {
+        title: '<h6>Lenda do Martelo Arreboladizo</h6',
+        location: {
+            lat: 41.9302435,
+            lng: -7.632325
+        }
+    },
+    {
+        title: '<h6>Lenda da Raíña Loba</h6',
+        location: {
+            lat: 41.8987155,
+            lng: -7.6711795
+        }
+    },
+    {
+        title: '<h6>Forno da Xironda</h6',
+        location: {
+            lat: 41.8997596,
+            lng: -7.6370162
+        }
+    },
+    {
+        title: '<h6>Peto de Ánimas da Xironda</h6',
+        location: {
+            lat: 41.8991647,
+            lng: -7.6366219
+        }
+    },
+    {
+        title: '<h6>Pombal</h6',
+        location: {
+            lat: 41.9002338,
+            lng: -7.6380408
+        }
+    },
+    {
+        title: '<h6>Cruceiro de Montes</h6',
+        location: {
+            lat: 41.9624469,
+            lng: -7.5568584
+        }
+    },
+    {
+        title: '<h6>Muíño do río de Montes</h6',
+        location: {
+            lat: 41.9685646,
+            lng: -7.5546616
+        }
+    },
+    {
+        title: '<h6>A Casa da Neve</h6',
+        location: {
+            lat: 41.9508133,
+            lng: -7.6404387
+        }
+    },
+    {
+        title: '<h6>Forno da Saceda</h6',
+        location: {
+            lat: 41.9352209,
+            lng: -7.6305628
+        }
+    },
+    {
+        title: '<h6>Lagar rupestre do Trugán</h6',
+        location: {
+            lat: 41.889641,
+            lng: -7.5895545
+        }
+    },
+    {
+        title: '<h6>Lagar rupestre da Baira 1</h6',
+        location: {
+            lat: 41.887181,
+            lng: -7.5925076
+        }
+    },
+    {
+        title: '<h6>Lagar rupestre da Baira 2</h6',
+        location: {
+            lat: 41.8876942,
+            lng: -7.5926283
+        }
+    },
+    {
+        title: '<h6>Lagar rupestre da Baira </h6',
+        location: {
+            lat: 41.8878919,
+            lng: -7.5934947
+        }
+    },
+    {
+        title: '<h6>Lagar rupestre de San Martiño</h6',
+        location: {
+            lat: 41.9517309,
+            lng: -7.5607556
+        }
+    },
+    {
+        title: '<h6>Peto de Ánimas</h6',
+        location: {
+            lat: 41.953191,
+            lng: -7.5607556
+        }
+    },
+    {
+        title: '<h6>O Penedo que Toca</h6',
+        location: {
+            lat: 41.9484673,
+            lng: -7.6373005
+        }
+    },
+    {
+        title: '<h6>Forno Comunal</h6',
+        location: {
+            lat: 41.9941079,
+            lng: -7.564508
+        }
+    },
+    {
+        title: '<h6>Forno Comunal</h6',
+        location: {
+            lat: 41.9934082,
+            lng: -7.5400731
+        }
+    },
+    {
+        title: '<h6>Cruceiro da Penela</h6',
+        location: {
+            lat: 41.9532309,
+            lng: -7.6189864
+        }
+    },
+    {
+        title: '<h6>Fonte de ?? </h6',
+        location: {
+            lat: 1.9530594,
+            lng: -7.6202926
+        }
+    },
+    {
+        title: '<h6>Praza da Picota</h6',
+        location: {
+            lat: 41.8992046,
+            lng: -7.6376022
+        }
+    },
+
+    {
+        title: '<h6>Área recreativa da Chaira</h6>',
+        location: {
+            lat: 41.8984959,
+            lng: -7.6716596
+        }
+    },
+    {
+        title: '<h6>Área recreativa dos Muíños</h6>',
+        location: {
+            lat: 41.9694022,
+            lng: -7.5564104
+        }
+    },
+    {
+        title: '<h6>Penedos de Pena Muller</h6>',
+        location: {
+            lat: 41.9466081,
+            lng: -7.6437646
+        }
+    },
+    {
+        title: '<h6>A Lama da Saceda</h6>',
+        location: {
+            lat: 41.929126,
+            lng: -7.6321507
+        }
+    }
+];
+var map;
+var markers = [];
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 41.9764254,
+            lng: -7.5953212
+        },
+        zoom: 11,
+        fullscreenControl: false,
+        gestureHandling: 'greedy',
+        mapTypeId: 'satellite',
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.RIGHT_TOP
+        }
+
+    });
+    var mainIcon = './assets/files/img/mainIcon.png';
+    var secondaryIcon = './assets/files/img/secondaryIcon.png';
+
+    var largeInfowindow = new google.maps.InfoWindow();
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        var icon = locations[i].icon;
+        var link = locations[i].link;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            //animation: google.maps.Animation.DROP,
+            id: i,
+            icon: icon,
+            zIndex: 6,
+            link: link
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open the large infowindow at each marker.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
+    }
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        // Clear the infowindow content to give the streetview time to load.
+        infowindow.setContent(marker.title + marker.link);
+        infowindow.marker = marker;
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+        });
+        // Open the infowindow on the correct marker.
+        infowindow.open(map, marker);
+    }
+}
+
+$(".Menu-link").on("click", function() {
+    $(".Menu-nav").toggleClass("active");
+    $(this).toggleClass("active");
+    if ($(".Menu-nav-lang").hasClass("active")) {
+        $(".Menu-nav-lang").removeClass("active");
+    }
+
+});
+
+$(".Menu-link-lang").on("click", function() {
+    $(".Menu-nav-lang").toggleClass("active");
+    $(this).toggleClass("active");
+    if ($(".Menu-nav").hasClass("active")) {
+        $(".Menu-nav").removeClass("active");
+    }
+});
+
+$('.MapHandler').click(function() {
+    if ($('#map').hasClass('Map-container--min') ||
+        $('#map').hasClass('Map-container--max') ||
+        $('#content').hasClass('Content-container--min') ||
+        $('#content').hasClass('Content-container--max')) {
+        $('#map').toggleClass('Map-container--max');
+        $('#content').toggleClass('Content-container--min');
+    } else {
+        $('#map').toggleClass('min');
+        $('#content').toggleClass('max');
+    }
+    setTimeout(function() {
+        if (map) {
+            var center = map.getCenter()
+            google.maps.event.trigger(map, 'resize', function() {
+                map.setCenter(center)
+            })
+        }
+    }, 400);
+});
+
+window.initMap = initMap;
